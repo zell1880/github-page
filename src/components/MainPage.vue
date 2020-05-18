9686b3e863f18f6a9649b4ab164bd983fe7c8bcf
Lim Zi Hao <lzhao1880@gmail.com>
2020-05-18 17:52:04 +0800

    Created MainPage

---
+++ src/components/MainPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <b-jumbotron class="my-5" bg-variant="dark" text-variant="white" border-variant="light">
+      <template v-slot:header>Lim Zi Hao</template>
+
+      <template v-slot:lead>
+        A fresh graduate looking for something to do.
+      </template>
+
+      <hr class="my-4 border-white">
+
+      <p>
+        Links to be added here:<br>
+        Linkedin | Github
+      </p>
+    </b-jumbotron>
+  </div>
+</template> 
+
+<style >
+</style>
