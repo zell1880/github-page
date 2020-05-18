b988ac63ce228d48bdce0e146783be60f8b8a3c8
Lim Zi Hao <lzhao1880@gmail.com>
2020-05-18 17:51:51 +0800

    Created Navbar

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <b-navbar  toggleable="sm" type="dark" variant="dark" fixed="bottom" >
+      <b-navbar-brand>
+      Just A Empty Portfolio
+      </b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item class="px-2"><b-icon-person/> About </b-nav-item>
+        <b-nav-item class="px-2"><b-icon-clock/> Timeline </b-nav-item>
+        <b-nav-item class="px-2"><b-icon-tools/> Skills </b-nav-item>
+        <b-nav-item class="px-2"><b-icon-card-checklist/> ToDo </b-nav-item>
+      </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<style>
+
+</style>
