7772044527b76dc349d01e5eb42d7ced7a8114b7
Lim Zi Hao <lzhao1880@gmail.com>
2020-05-19 16:16:15 +0800

    added router links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,9 +7,9 @@
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav class="ml-auto">
-        <b-nav-item class="px-2"><b-icon-person/> About </b-nav-item>
-        <b-nav-item class="px-2"><b-icon-clock/> Timeline </b-nav-item>
-        <b-nav-item class="px-2"><b-icon-tools/> Skills </b-nav-item>
+        <b-nav-item to="/about" class="px-2"><b-icon-person/> About </b-nav-item>
+        <b-nav-item to="/timeline" class="px-2"><b-icon-clock/> Timeline </b-nav-item>
+        <b-nav-item to="/skills"><b-icon-tools/> Skills </b-nav-item>
         <b-nav-item class="px-2"><b-icon-card-checklist/> ToDo </b-nav-item>
       </b-navbar-nav>
       </b-collapse>
