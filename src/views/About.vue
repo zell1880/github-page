2158806deafb53440b4fad7cedfe2f1386da4ccb
Lim Zi Hao <lzhao1880@gmail.com>
2020-05-19 16:18:40 +0800

    created views

---
+++ src/views/About.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <div class="body">
+      <MainPage/>
+    </div>
+  </div>
+</template>
+<script>
+import MainPage from '@/components/MainPage.vue'
+
+export default {
+  components: {
+    MainPage
+  }
+}
+</script>
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  width:100%;
+  margin: 0px 0px 0px 0px;
+}
+.body {
+  width: 100%;
+  height: 100%;
+  padding-top:40px;
+  padding-bottom:40px
+}
+</style>
