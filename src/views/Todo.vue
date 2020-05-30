be8f9edda810c6c66f9203ec23e0cb54610e3f03
Lim Zi Hao <lzhao1880@gmail.com>
2020-05-30 14:01:30 +0800

    created todo.vue

---
+++ src/views/Todo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="my-5">
+    <b-jumbotron class="" bg-variant="dark" text-variant="white" border-variant="light">
+      
+    </b-jumbotron>
+    <span class="pt-5"/>
+  </div>
+</template>
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  computed: mapGetters(['allTodos'])
+}
+</script>>
