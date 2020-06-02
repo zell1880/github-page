eebfee1485430610adf8a938ce04e81481f12282
zell1880 <lzhao1880@gmail.com>
2020-06-02 17:44:50 +0800

    added store

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
@@ -14,6 +15,7 @@ Vue.use(IconsPlugin)
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   router,
   render: h => h(App)
 }).$mount('#app')
